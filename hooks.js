cac0f7a8ad67fb4f137fcb9d9001952b234bc451
John McLear <john@mclear.co.uk>
2016-05-20 14:42:16 +0100

    Bump Version number and publish to NPM

--- hooks.js
+++ hooks.js
@@ -1,9 +1,13 @@
 exports.eejsBlock_customStyles = function (hook_name, args, cb) {
-  args.content = "";
+  args.content = "<!-- Removed Custom Styles by ep_disable_custom_scripts plugin -->";
   return cb();
 };
 
 exports.eejsBlock_customScripts = function (hook_name, args, cb) {
-  args.content = "";
+  args.content = "<!-- Removed Custom Scripts by ep_disable_custom_scripts plugin -->";
   return cb();
+};
+
+exports.clientVars = function (hook, context, callback){
+  return callback( { "disableCustomScriptsAndStyles": true } );
 };
