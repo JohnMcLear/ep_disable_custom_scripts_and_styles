68b05b0a2dbc0cc176d66e5ea47af51885c55b8d
John McLear <john@mclear.co.uk>
2021-01-07 22:36:42 +0000

    autofixes from Etherpad checkPlugins.js

--- hooks.js
+++ hooks.js
@@ -1,13 +1,13 @@
 exports.eejsBlock_customStyles = function (hook_name, args, cb) {
-  args.content = "<!-- Removed Custom Styles by ep_disable_custom_scripts plugin -->";
+  args.content = '<!-- Removed Custom Styles by ep_disable_custom_scripts plugin -->';
   return cb();
 };
 
 exports.eejsBlock_customScripts = function (hook_name, args, cb) {
-  args.content = "<!-- Removed Custom Scripts by ep_disable_custom_scripts plugin -->";
+  args.content = '<!-- Removed Custom Scripts by ep_disable_custom_scripts plugin -->';
   return cb();
 };
 
-exports.clientVars = function (hook, context, callback){
-  return callback( { "disableCustomScriptsAndStyles": true } );
+exports.clientVars = function (hook, context, callback) {
+  return callback({disableCustomScriptsAndStyles: true});
 };
